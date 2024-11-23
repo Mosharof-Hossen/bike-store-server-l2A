6a8418ead15298b0685660a74d799329561bdc2f
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-11-23 12:35:08 +0600

    basic setup done and database connected

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+
+async function server() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Server is running on ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+server();
